5f1d533146c079c2ef093c990dbaaf6e8c98f557
Pixel Lemons <164111072+LevinShao@users.noreply.github.com>
2024-09-23 21:43:33 +1000

    9.0

---
+++ Epic Block Run/Assets/Scripts/FPSLimiter.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FPSLimiter : MonoBehaviour
+{
+    public int targetFPS = 60;
+    
+    void Start()
+    {
+        QualitySettings.vSyncCount = 0;
+        Application.targetFrameRate = targetFPS;
+    }
+}
+
